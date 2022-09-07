0fa07ea890e4e49ca3f0f235209d0287f1fee0d9
RAVI SHANKAR PRATIHAST <75324945+RaviPratihast@users.noreply.github.com>
2022-09-07 11:29:06 +0530

    add
    done with implementing program.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 
 const circle=document.querySelector('.circle');
 
+const elementContainer=document.querySelector('.rotate-container');
+
 const rotate=document.querySelector('.btn-rotate');
 
 const close=document.querySelector('.cancel-rotate');
@@ -13,8 +15,11 @@ console.log(circle,rotate,close);
 
 rotate.addEventListener('click',()=>{
     circle.classList.add('circle-rotate');
+
+    elementContainer.classList.add('circle-container-rotate');
 });
 
 close.addEventListener('click',()=>{
     circle.classList.remove('circle-rotate');
+    elementContainer.classList.remove('circle-container-rotate');
 })
